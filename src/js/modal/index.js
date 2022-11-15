95255689d91fd6ff1390f3ffca5c8269dd25beb9
Alina Romanova <alinakhraban@gmail.com>
2022-11-15 14:55:50 +0200

    Add edits

--- src/js/modal/index.js
+++ src/js/modal/index.js
@@ -1,16 +1,15 @@
 // import './sass/modal.scss';
 import { API } from '../utils/api';
-// import createModalCard from '..templates/modal.hbs';
+import createModalCard from '../../templates/modal.hbs';
 
 const modalEl = document.querySelector('.modal');
 
 
 const api = new API();
 
-api.getMovieById()
-.then(({data}) => {
+api.getMovieById(505642)
+.then((data) => {
  console.log(data)
-})
-// modalEl.innerHTML = createModalCard(data)
 
- api.getPopularMovies().then(e=>{console.log(e)})
+ modalEl.innerHTML = createModalCard(data)
+})
