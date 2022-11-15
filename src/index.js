2af6a383b7b7a6c770cb5d77fa876df76b6983b9
Ivan Drahoner <idrahoner@gmail.com>
2022-11-16 01:13:28 +0200

    Add open modal on card click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import * as hesderJS from './js/header/index';
-// import { initLoad } from './js/gallery';
 
 import { LocalStorageAPI } from './js/utils/local-storage-api';
 import { API } from './js/utils/api';
@@ -9,18 +8,6 @@ const QUEUE_MOVIE_KEY = 'queueMovieList';
 
 const watchedMovieStore = new LocalStorageAPI(WATCHED_MOVIE_KEY);
 const queueMovieStore = new LocalStorageAPI(QUEUE_MOVIE_KEY);
-
-// initLoad();
-
-// Приклад роботи модалки:
-
-import { showMovieDetails } from './js/modal/modal';
-
-const openModalBtn = document.querySelector('.open-modal');
-
-openModalBtn.addEventListener('click', () => {
-  showMovieDetails(505642);
-});
 
 // Приклад роботи галереї:
 
