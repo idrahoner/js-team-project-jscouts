89245a9c748683b25508848cf7d8beb25aac02fa
Ivan Drahoner <idrahoner@gmail.com>
2022-11-15 19:20:23 +0200

    some refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import * as hesderJS from './js/header/index';
 import { initLoad } from './js/gallery';
 
+import { LocalStorageAPI } from './js/utils/local-storage-api';
+
+const WATCHED_MOVIE_KEY = 'wathcedMovieList';
+const QUEUE_MOVIE_KEY = 'queueMovieList';
+
+const watchedMovieStore = new LocalStorageAPI(WATCHED_MOVIE_KEY);
+const queueMovieStore = new LocalStorageAPI(QUEUE_MOVIE_KEY);
+
 initLoad();
