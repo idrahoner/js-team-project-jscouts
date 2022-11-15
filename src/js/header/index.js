304dade74f5e2a0b09f4619611e858a63b31633d
leonbahley <leon.bahley@icloud.com>
2022-11-15 20:18:55 +0100

    add-search-render

--- src/js/header/index.js
+++ src/js/header/index.js
@@ -2,6 +2,7 @@ import { API } from '../utils/api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import card from '../../templates/card.hbs';
 
+const gallery = document.querySelector('.gallery');
 export const loader = document.querySelector('.loader');
 export const header = document.querySelector('.header');
 export const headerWrapper = document.querySelector('.header__wrapper');
@@ -49,6 +50,21 @@ export function handleSubmit(event) {
       if (data.results.length === 0) {
         Notify.failure('No such movie');
       }
+      gallery.innerHTML = card(data.results);
+      obj.getGenreList().then(genres => {
+        const d = document.querySelectorAll('.templates-film__text');
+        for (let i = 0; i < data.results.length; i++) {
+          const r = data.results[i].genre_ids;
+          for (let j = 0; j < genres.length; j++) {
+            if (r.includes(genres[j].id)) {
+              d[i].innerHTML +=
+                ' ' + genres[j].name + ' <span class="span"> , </span> ';
+            }
+          }
+          const dat = new Date(data.results[i].release_date);
+          d[i].innerHTML += '| ' + dat.getFullYear();
+        }
+      });
     })
     .catch(console.log)
     .then(() => loader.classList.toggle('loader-hidden'));
