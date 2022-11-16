d9c028a68d6202b7d02ddc9b5249ffe9941a0762
Alina Romanova <alinakhraban@gmail.com>
2022-11-16 16:33:16 +0200

    Add edits

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,4 +1,5 @@
 import { API } from '../utils/api';
+import Notiflix from 'notiflix';
 import modalMarkup from '../../templates/modal.hbs';
 import {LocalStorageAPI, QUEUE_MOVIE_KEY, WATCHED_MOVIE_KEY } from '../utils/local-storage-api'
 
@@ -43,6 +44,15 @@ export async function showMovieDetails(movieId) {
  movieData.genres = movieData.genres.map(e=> e.name).join(", ")
  modalBodyEl.innerHTML = modalMarkup(movieData);
 
+
+//  if(addQueueMovie.getItems().find(el => el.id === movieId)) {
+
+//   modalBtnQueueMovie.disabled = true;
+//   Notiflix.Notify.info('This movie has been added')
+//  }
+
+//  movieData.find(el =>  )
+
   const modalBtnWatchedMovie = document.querySelector('.modal__btn-watched');
   const modalBtnQueueMovie = document.querySelector('.modal__btn-queue');
 
@@ -52,11 +62,27 @@ export async function showMovieDetails(movieId) {
 
   function onAddWatchedMovie(e) {
     addWatchedMovie.saveObject(movieData)
+
+    if(addWatchedMovie.getItems(movieId)) {
+      modalBtnWatchedMovie.disabled = true;
+      document.querySelector('.modal__btn-watched').textContent = "Added watched";
+      document.querySelector('.modal__btn-watched').style.backgroundColor = " #FF6B01"
+      document.querySelector('.modal__btn-watched').style.color = "white"
+      document.querySelector('.modal__btn-watched').style.border= "transparent"
+     }
   };
 
-  function  onAddQueueMovie(e) {
+
+  function onAddQueueMovie(e) {
     addQueueMovie.saveObject(movieData)
 
-  };
+    if(addQueueMovie.getItems(movieId)) {
+      modalBtnQueueMovie.disabled = true;
+      document.querySelector('.modal__btn-queue').textContent = "Added to queue"
+      document.querySelector('.modal__btn-queue').style.backgroundColor = " #FF6B01"
+      document.querySelector('.modal__btn-queue').style.color = "white"
+      document.querySelector('.modal__btn-queue').style.border= "transparent"
+     }
+  }
 
 }
